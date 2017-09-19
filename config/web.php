ad1e529ae604bb4a016a16840b7a0360e9d31825
lando <lando@lando.local>
2017-09-19 23:23:41 +0700

    modify

--- config/web.php
+++ config/web.php
@@ -10,7 +10,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => '-54_EHuCJHZLaTiOyy3owc3BqcayyBes',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
